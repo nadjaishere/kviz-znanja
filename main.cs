f236c72a95d2aca0929f1ace2be0097d99d0cc9d
Надежда Ристић <104567887+nadjaishere@users.noreply.github.com>
2022-11-08 09:58:01 +0000

    Metoda za upis pitanja,ponudjenog odgovora,indeksa tacnog odgovara koje zadaje korisnik u bilo kom trenutku.

--- main.cs
+++ main.cs
@@ -1,7 +1,17 @@
 using System;
+using System.IO;
 
 class Program {
-  public static void Main (string[] args) {
+  static void UpisPodataka(string ime_datoteke_kategorije,string pitanje,string[] ponudjeni_odgovori,int 
+                           indeks_tacnog_odg)
+        {
+            StreamWriter upis = new StreamWriter(ime_datoteke_kategorije, true);
+            upis.WriteLine(pitanje+"|"+ponudjeni_odgovori[0]+ "|"+ ponudjeni_odgovori[1]+ "|"+ 
+            ponudjeni_odgovori[2]+ "|"+ponudjeni_odgovori[3]+ "|"+indeks_tacnog_odg);
+            upis.Close();
+        }
+  public static void Main (string[] args) 
+  {
     Console.WriteLine ("Hello World");
   }
 }
