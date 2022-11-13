d858b678d3215958881930cf1995def3da95dac2
Надежда Ристић <104567887+nadjaishere@users.noreply.github.com>
2022-11-13 11:25:27 +0000

    -dodate metode i kod u main-u za random izbor pitanja(tako da se ne ponavljaju pitanja) -potpuno sredjen unos (treba testirati metodu za izlazak iz programa ,upis pitanja i videti kako tada radi unos)

--- main.cs
+++ main.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 class Program
 {
-  struct Pitanja
+ struct Pitanja
         {
             public string[] pitanje;
             public string[] odgovor1;
@@ -11,110 +11,111 @@ class Program
             public string[] odgovor3;
             public string[] odgovor4;
             public int[] indeks_tacnog_odg;
-            public int broj_pitanja;
+            public int brojpitanja;
         }
-    static int kursorX = 0, kursorY = 0;
-  //koliko polja mogu da idem gore dole
-  //x=0 i y=4
-  //
-  
-    static void PomerajKursora(int x, int y, int pomerajX, int pomerajY)
-    {
-        kursorX = 0;
-        kursorY = 0;
-        int pocetniY = Console.CursorTop;
-        int pocetniX = Console.CursorLeft;
-        ConsoleKeyInfo strelica;
-        do
-        {
-            strelica = Console.ReadKey(true);
-            if (strelica.Key == ConsoleKey.DownArrow)
-            {
-                if (kursorY >= 0 && kursorY < y * pomerajY) kursorY += pomerajY;
-            }
-            else if (strelica.Key == ConsoleKey.UpArrow)
-            {
-                if (kursorY > 0 && kursorY <= y * pomerajY) kursorY -= pomerajY;
-            }
-            else if (strelica.Key == ConsoleKey.LeftArrow)
-            {
-                if (kursorX > 0 && kursorX <= x * pomerajX) kursorX -= pomerajX;
-            }
-            else if (strelica.Key == ConsoleKey.RightArrow)
-            {
-                if (kursorX >= 0 && kursorX < x * pomerajX) kursorX += pomerajX;
-            }
-            else if (strelica.Key == ConsoleKey.Enter)
-            {
-                break;
-            }
-            else continue;
-            Console.SetCursorPosition(pocetniX + kursorX, pocetniY + kursorY);
-        }
-        while (true);
-    }
-  //
-  //
-    static void UpisPitanja(string[] kategorije)
-    {
-        Console.WriteLine("Koliko pitanja zelite da unesete?");
-        int brojpitanja;
-        while (!int.TryParse(Console.ReadLine(), out brojpitanja) || brojpitanja < 0)
-        {
-            Console.WriteLine("Unesite ponovo broj pitanja");
-        }
-        for (int i = 0; i < brojpitanja; i++)
-        {
-            Console.WriteLine("Unesite ime kategorije kojoj zelite da dodelite pitanje koje unostite:");
-            string ime_datoteke_kategorije = Console.ReadLine().ToLower();
-            Console.WriteLine("Unesite pitanje");
-            string pitanje = Console.ReadLine();
-            Console.WriteLine("Unesite 4 ponudjena odgovora razdvojeni jednim razmakom");
-            string[] ponudjeni_odgovori = Console.ReadLine().Split();
-            while (ponudjeni_odgovori.Length != 4)
-            {
-                Console.WriteLine("Unesite ponovo ponudjene odgovore ponovo");
-                ponudjeni_odgovori = Console.ReadLine().Split();
-            }
-            Console.WriteLine("Unesite indeks tacnog odgovora (broj od 1 do 4):");
-            int indeks;
-            while (!int.TryParse(Console.ReadLine(), out indeks) || indeks < 1 || indeks > 4)
-            {
-                Console.WriteLine("Unesite ponovo indeks tacnog odgovora ponovo");
-            }
-            StreamWriter upis = new StreamWriter(ime_datoteke_kategorije, true);
-            upis.WriteLine(pitanje + "|" + ponudjeni_odgovori[0] + "|" + ponudjeni_odgovori[1] + "|" + ponudjeni_odgovori[2] + "|" + ponudjeni_odgovori[3] + "|" + indeks);
-            upis.Close();
-        }
+        static int kursorX = 0, kursorY = 0;
+        //koliko polja mogu da idem gore dole
+        //x=0 i y=4
+        //
 
-    }
-  //
-  //
-  static void UnosPitanjaIzDatoteke(string ime_kategorije,int broj_kategorije)
+        static void PomerajKursora(int x, int y, int pomerajX, int pomerajY)
         {
-            Pitanja[] kat = new Pitanja[1000];
-            int brojac_pitanja = 0;
-            if (File.Exists(ime_kategorije))
+            kursorX = 0;
+            kursorY = 0;
+            int pocetniY = Console.CursorTop;
+            int pocetniX = Console.CursorLeft;
+            ConsoleKeyInfo strelica;
+            do
             {
-                StreamReader ulaz = new StreamReader(ime_kategorije);
+                strelica = Console.ReadKey(true);
+                if (strelica.Key == ConsoleKey.DownArrow)
+                {
+                    if (kursorY >= 0 && kursorY < y * pomerajY) kursorY += pomerajY;
+                }
+                else if (strelica.Key == ConsoleKey.UpArrow)
+                {
+                    if (kursorY > 0 && kursorY <= y * pomerajY) kursorY -= pomerajY;
+                }
+                else if (strelica.Key == ConsoleKey.LeftArrow)
+                {
+                    if (kursorX > 0 && kursorX <= x * pomerajX) kursorX -= pomerajX;
+                }
+                else if (strelica.Key == ConsoleKey.RightArrow)
+                {
+                    if (kursorX >= 0 && kursorX < x * pomerajX) kursorX += pomerajX;
+                }
+                else if (strelica.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                else continue;
+                Console.SetCursorPosition(pocetniX + kursorX, pocetniY + kursorY);
+            }
+            while (true);
+        }
+        //
+        //
+        static void UpisPitanja(string[] kategorije)
+        {
+            Console.WriteLine("Koliko pitanja zelite da unesete?");
+            int brojpitanja;
+            while (!int.TryParse(Console.ReadLine(), out brojpitanja) || brojpitanja < 0)
+            {
+                Console.WriteLine("Unesite ponovo broj pitanja");
+            }
+            for (int i = 0; i < brojpitanja; i++)
+            {
+                Console.WriteLine("Unesite ime kategorije kojoj zelite da dodelite pitanje koje unostite:");
+                string ime_datoteke_kategorije = Console.ReadLine().ToLower();
+                Console.WriteLine("Unesite pitanje");
+                string pitanje = Console.ReadLine();
+                Console.WriteLine("Unesite 4 ponudjena odgovora razdvojeni jednim razmakom");
+                string[] ponudjeni_odgovori = Console.ReadLine().Split();
+                while (ponudjeni_odgovori.Length != 4)
+                {
+                    Console.WriteLine("Unesite ponovo ponudjene odgovore ponovo");
+                    ponudjeni_odgovori = Console.ReadLine().Split();
+                }
+                Console.WriteLine("Unesite indeks tacnog odgovora (broj od 1 do 4):");
+                int indeks;
+                while (!int.TryParse(Console.ReadLine(), out indeks) || indeks < 1 || indeks > 4)
+                {
+                    Console.WriteLine("Unesite ponovo indeks tacnog odgovora ponovo");
+                }
+                StreamWriter upis = new StreamWriter(ime_datoteke_kategorije, true);
+                upis.WriteLine(pitanje + "|" + ponudjeni_odgovori[0] + "|" + ponudjeni_odgovori[1] + "|" + ponudjeni_odgovori[2] + "|" + ponudjeni_odgovori[3] + "|" + indeks);
+                upis.Close();
+            }
+
+        }
+        //
+        //
+        static void UnosPitanjaIzDatoteke(string ime_kategorije, int broj_kategorije,Pitanja[] kat)
+        {
+            int brojac_pitanja = 0;
+            StreamReader ulaz = new StreamReader(ime_kategorije+".txt");
                 while (!ulaz.EndOfStream)
                 {
-                    string s = ulaz.ReadLine();
-                    string[] red = s.Split();
-                    kat[broj_kategorije].pitanje[brojac_pitanja] = red[0];
-                    kat[broj_kategorije].odgovor1[brojac_pitanja] = red[1];
-                    kat[broj_kategorije].odgovor2[brojac_pitanja] = red[2];
-                    kat[broj_kategorije].odgovor3[brojac_pitanja] = red[3];
-                    kat[broj_kategorije].odgovor4[brojac_pitanja] = red[4];
-                    kat[broj_kategorije].indeks_tacnog_odg[brojac_pitanja] = Convert.ToInt32(red[5]);
-                    brojac_pitanja++;
+                kat[broj_kategorije].pitanje = new string[1000];
+                kat[broj_kategorije].odgovor1 = new string[1000];
+                kat[broj_kategorije].odgovor2 = new string[1000];
+                kat[broj_kategorije].odgovor3 = new string[1000];
+                kat[broj_kategorije].odgovor4 = new string[1000];
+                kat[broj_kategorije].indeks_tacnog_odg = new int[1000];
+                string s = ulaz.ReadLine();
+                string[] red = s.Split('|');
+                kat[broj_kategorije].pitanje[brojac_pitanja] = red[0];
+                kat[broj_kategorije].odgovor1[brojac_pitanja] = red[1];
+                kat[broj_kategorije].odgovor2[brojac_pitanja] = red[2];
+                kat[broj_kategorije].odgovor3[brojac_pitanja] = red[3];
+                kat[broj_kategorije].odgovor4[brojac_pitanja] = red[4];
+                kat[broj_kategorije].indeks_tacnog_odg[brojac_pitanja] = Convert.ToInt32(red[5]);
+                brojac_pitanja++;
                 }
-              kat[broj_kategorije].brojpitanja = brojac_pitanja;
-            }
+                kat[broj_kategorije].brojpitanja = brojac_pitanja;
         }
-  //
-  //
-    static bool PrekidKvizaKorisnik(ConsoleKeyInfo x, string[] kategorije)
+        //
+        static bool PrekidKvizaKorisnik(ConsoleKeyInfo x, string[] kategorije,Pitanja[] kat)
         {
             if (x.Key == ConsoleKey.Escape)
             {
@@ -125,21 +126,43 @@ class Program
                 UpisPitanja(kategorije);
                 for (int i = 0; i < 20; i++)
                 {
-                    UnosPitanjaIzDatoteke(kategorije[i], i);
+                    UnosPitanjaIzDatoteke(kategorije[i], i,kat);
                 }
             }
             return false;
         }
-  //
-  //
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World");
-        //ConsoleKeyInfo-tip podataka
-        //ConsoleKeyInfo=Console.ReadKey
-        //ConsoleKey.Escape
-        //ConsoleKey.D1
-        string[] kategorije = {
+        //
+        static int IndeksKategorije(string kategorija,string[] kategorije)
+        {
+            int indeks_kategorije = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                if (kategorija==kategorije[i])
+                {
+                    indeks_kategorije = i + 1;
+                }
+            }
+            return indeks_kategorije;
+        }
+        static bool ProveraPonavljanjaPitanja(int broj_pitanja,int[] brojevi_pitanja)
+        {
+            for (int i = 0; i < brojevi_pitanja.Length; i++)
+            {
+                if (broj_pitanja==brojevi_pitanja[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            //ConsoleKeyInfo-tip podataka
+            //ConsoleKeyInfo=Console.ReadKey
+            //ConsoleKey.Escape
+            //ConsoleKey.D1
+            string[] kategorije = {
             "Sport",
             "Filmovi i serije",
             "Moda",
@@ -160,7 +183,26 @@ class Program
             "Nauka",
             "Poznate licnosti",
             "Razno"
-        };
-      
-    }
+            };
+            Console.WriteLine("KVIZ ZNANJA");
+            Console.WriteLine("Izaberite kategoriju:");
+            string katego = Console.ReadLine();
+            int broj_kategorije = IndeksKategorije(katego, kategorije);
+            Pitanja[] kat = new Pitanja[1000];
+            UnosPitanjaIzDatoteke(katego, broj_kategorije,kat);
+            Console.WriteLine(kat[broj_kategorije].brojpitanja);
+            //izbor random pitanja iz kategorije
+            int[] random_pitanja_indeksi = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                random_pitanja_indeksi[i] = rnd.Next(1, kat[broj_kategorije].brojpitanja);
+                while (ProveraPonavljanjaPitanja(random_pitanja_indeksi[i],random_pitanja_indeksi))
+                {
+                    random_pitanja_indeksi[i] = rnd.Next(1, kat[broj_kategorije].brojpitanja);
+                }
+            }
+
+
+        }
 }
